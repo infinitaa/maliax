1b0dc6263ce183cc23b9c14ecb5db20b01469d96
infinitaa <emad.pourshirazi@gmail.com>
2024-03-02 12:52:12 +0000

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Maliax',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
